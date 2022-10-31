d4298becac3ababf94f25b295360e650cf46b560
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-10-31 22:07:44 +0100

    connect to MongoDB

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connection from "./database/db.js";
+
 dotenv.config();
 
 const app = express();
@@ -8,6 +10,7 @@ const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+connection();
 
 app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
